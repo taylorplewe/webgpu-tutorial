514cc210ee284140d88e992d7ec909c3c27330ee
Taylor Plewe <taylor.plewe@awardco.com>
2024-05-17 13:06:44 -0600

    first page of tutorial

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// DELETE IF YOU CAN'T FIGURE OUT HOW TO SERVE FILES AS NOT text/html
 const canvasEl = document.querySelector('canvas');
 if (!navigator.gpu) {
 	throw new Error('WebGPU not supported in this browser!!');
